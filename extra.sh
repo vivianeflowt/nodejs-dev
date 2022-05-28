ec747e0e9f7ea4e76e8578d3d334ca56384da07d
vivianeflowt <vivianeflowt@gmail.com>
2022-05-28 04:26:32 -0300

    step

--- extra.sh
+++ extra.sh
@@ -1,55 +1,5 @@
 #!/bin/bash
 
-cd ~
-
-zsh
-
-curl -L git.io/antigen > antigen.zsh
-source antigen.zsh
-
-antigen init 
-antigen bundle git
-antigen bundle pip
-antigen bundle python
-antigen bundle command-not-found
-antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-autosuggestions
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen theme spaceship-prompt/spaceship-prompt
-antigen apply
-
-
-echo ""
-echo "------------------------------- INSTALL - NVM ------------------------------"
-echo ""
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-wait
-
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# nvm ls-remote
-
-nvm install node
-nvm install-latest-npm
-nvm install --lts
-
-nvm install v10.24.1
-nvm install v12.22.12
-nvm install v14.19.2
-
-nvm install v16.15.0
-nvm use v16.15.0
-nvm use default v16.15.0
-
-echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"'>> ~/.bashrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"'>> ~/.bashrc
-
-echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"'>> ~/.zshrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"'>> ~/.zshrc
-
-
 echo ""
 echo "------------------------------- UBUNTU -----------------------------------"
 echo ""  
